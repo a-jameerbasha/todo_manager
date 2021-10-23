6629ddd129b88c2e20085fd8c755e7f31ed5513c
a-jameerbasha <jameer@hit.edu.in>
2021-10-23 18:50:24 +0530

    Route Update

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   #get "todos", to: "todos#index"
   #post "todos/create", to:"todos#create"
   #get "todos/:id", to:"todos#show"
-
   post "users/login", to: "users#login"
 
   resources :todos
